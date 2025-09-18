7a602d086f84ac1014b79a5617784225ff766e48
ullola_1710 <jadeullola@icloud.com>
2025-09-17 20:35:55 -0600

    Resolución de practica

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,12 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
+const input = document.getElementById("input");
+const btn = document.getElementById("button");
+const answer = document.getElementById("answer"); 
+const error = document.getElementById("error");
+let timer = null;
+
 /**
  * STEPS:
  *
@@ -11,3 +17,65 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+async function fetchAnswer(){
+    const question = (input.value || '').trim();
+    if(!question){
+        showError("Escribe una pregunta")
+        return;
+    } // if
+
+    loading(true);
+    clearMsg();
+    setAnswer('Asking the oracle...'); 
+
+    try{
+        const res = await fetch(API_ENDPOINT, {
+            cache: 'no-store'
+        });
+        if(!res.ok) throw new Error(`${res.status}`);
+
+        const data = await res.json();
+        const mayus = String(data.answer).toUpperCase();
+
+        setAnswer(mayus);
+        cleanAll(5000);
+    } catch (err) {
+        console.error(err);
+        showError('We have some issue to get the correct answer. Try again later');
+        setAnswer('');
+    } // try-catch
+    finally {
+        loading(false);
+    } // finally
+
+} // async fetchAnswer
+
+btn.addEventListener('click', fetchAnswer);
+
+
+function loading(isLoading) {
+    btn.disabled = isLoading;
+    btn.textContent = isLoading ? 'Searching...' : 'Get answer as soon as posible...'; 
+} // loading
+
+function setAnswer(text){
+    answer.texgittContent = text || ''; 
+} // setAnswer
+
+function showError(msg){
+    error.textContent = msg; 
+} // showError
+
+function clearMsg(){
+    error.textContent = ''; 
+} // clearMsg 
+
+function cleanAll(timeMs){
+    if(timer) clearTimeout(timer); 
+    timer = setTimeout(() => {
+        input.value = ''; 
+        setAnswer(''); 
+        clearMsg();
+    }, timeMs); 
+} // cleanAll
